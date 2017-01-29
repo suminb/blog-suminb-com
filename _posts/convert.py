11c1c097882fd20ad53fe50a6939e723776def24
Sumin Byeon <suminb@gmail.com>
2017-01-29 17:41:56 +0900

    Redirect to philosophical.one domain

--- _posts/convert.py
+++ _posts/convert.py
@@ -23,7 +23,7 @@ def replace_html_tags(text):
     return text
 
 
-def insert_redirect_predicate(text):
+def insert_redirect_from_predicate(text):
     s = re.search(r"post_id: '(\d+)'", text)
     post_id = s.group(1)
 
@@ -35,21 +35,60 @@ def insert_redirect_predicate(text):
     predicate = '''
 redirect_from:
   - /archives/{}/'''.format(post_id)
-    
+
     return prefix + predicate + suffix
 
 
-def insert_absolute_url(text):
-    return text.replace('src="/wp-content', 'src="http://blog.suminb.com/wp-content')
+def insert_redirect_to_predicate(text, filename):
+    index = text.find('\n---\n', 3)
 
-if __name__ == '__main__':
+    prefix = text[:index]
+    suffix = text[index:]
+
+    slug = basename_without_extension(filename)[11:]
+
+    predicate = '''
+redirect_to:
+  - http://philosophical.one/post/{}'''.format(slug)
+
+    return prefix + predicate + suffix
+
+
+
+def insert_absolute_url(text):
+    return text.replace(
+        'src="/wp-content',
+        'src="http://blog.suminb.com/wp-content')
+
+
+def basename_without_extension(full_path):
+    basename = os.path.basename(full_path)
+    return os.path.extsep.join(os.path.splitext(basename)[:-1])
+
+
+def migrate1():
     source_file_name = sys.argv[1]
     source_file_name_noext = os.path.splitext(source_file_name)[0]
     target_file_name = '{}.markdown'.format(source_file_name_noext)
 
     content = read_file(source_file_name)
     content = replace_html_tags(content)
-    content = insert_redirect_predicate(content)
+    content = insert_redirect_from_predicate(content)
     content = insert_absolute_url(content)
 
     write_file(target_file_name, content)
+
+
+def migrate2():
+    filename = sys.argv[1]
+
+    content = read_file(filename)
+    content = replace_html_tags(content)
+    content = insert_redirect_to_predicate(content, filename)
+
+    # Print to the standard output
+    print(content)
+
+
+if __name__ == '__main__':
+    migrate2()
